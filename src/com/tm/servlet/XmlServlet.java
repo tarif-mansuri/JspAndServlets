f4d9055b96c1d1c54ea85d08f487317cbb6dd00b
tarif.m <tarif.m@zohocorp.com>
2019-05-18 13:38:19 +0530

    Initial Commit

---
+++ src/com/tm/servlet/XmlServlet.java
@@ -0,0 +1,24 @@
+//$Id$
+package com.tm.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class XmlServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/htnl");
+		PrintWriter out = response.getWriter();
+		String userName = request.getParameter("userName");
+		out.println("Hello from the post method! "+userName);
+	}
+}
